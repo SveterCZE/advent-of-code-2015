f5181391a87a3bd5ac8a424f1536cd9d00158650
Petr Šmerkl <46304018+SveterCZE@users.noreply.github.com>
2022-03-09 22:35:27 +0100

    day 8

---
+++ day8/day8.py
@@ -0,0 +1,43 @@
+import ast
+import re
+
+def main():
+    instructions = get_input()
+    part1(instructions)
+    part2(instructions)
+    return 0
+
+def get_input():
+    f = open("input.txt", "r", encoding='utf-8')
+    instructions = []
+    for line in f:
+        instructions.append(line.strip())
+    return instructions
+
+def part1(instructions):
+    lenght_chars = 0
+    lenght_string = 0
+    for elem in instructions:
+        lenght_chars += len(elem)
+        lenght_string += len(ast.literal_eval(elem))
+    print(lenght_chars - lenght_string)
+    return 0
+
+def part2(instructions):
+    lenght_chars = 0
+    lenght_new_string = 0
+    for elem in instructions:
+        lenght_chars += len(elem)
+        temp_len = len(elem) + calculate_new_string_len(elem[1:-1]) + 4
+        lenght_new_string += temp_len
+    print(lenght_new_string - lenght_chars)
+    return 0
+
+def calculate_new_string_len(elem):
+    counter = 0
+    counter += elem.count('\\')
+    counter += elem.count('"')
+    return counter
+
+
+main()
